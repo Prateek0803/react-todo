c31ccd101ae625d79798a9a0a505469134e8722f
Prateek Chatterjee <pchatterjee136@gmail.com>
2020-07-23 11:04:33 +0530

    Updates

---
+++ precache-manifest.60dbeafcb7c39006507ccda29bb250ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b693351dd62ce2f49bcb6ff76108f82",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "0f495f41c5861240113d",
+    "url": "/react-todo/static/css/main.57fc3e8a.chunk.css"
+  },
+  {
+    "revision": "a3af79d8e116345f4903",
+    "url": "/react-todo/static/js/2.88bc45a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo/static/js/2.88bc45a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f495f41c5861240113d",
+    "url": "/react-todo/static/js/main.05e49610.chunk.js"
+  },
+  {
+    "revision": "3633c6b57eaa960b8095",
+    "url": "/react-todo/static/js/runtime-main.0f00abfb.js"
+  }
+]);
